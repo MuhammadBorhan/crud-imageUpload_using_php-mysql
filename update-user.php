0a3b8e57a5c5216e520c92a587890374c197098b
MuhammadBorhan <0317371981@student.wub.edu.bd>
2023-02-09 13:07:09 +0600

    user update successfully done

---
+++ update-user.php
@@ -0,0 +1,96 @@
+<?php include "header.php"; 
+
+if(isset($_POST['submit'])){
+
+    include 'config.php';
+
+    $user_id = mysqli_real_escape_string($connection,$_POST['user_id']);
+    $fname = mysqli_real_escape_string($connection,$_POST['f_name']);
+    $lname = mysqli_real_escape_string($connection,$_POST['l_name']);
+    $user = mysqli_real_escape_string($connection,$_POST['username']);
+    $role = mysqli_real_escape_string($connection,$_POST['role']);
+
+    $updateUser = "UPDATE user SET 
+    first_name = '{$fname}', 
+    last_name = '{$lname}',
+    username = '{$user}',
+    role = '{$role}' WHERE user_id = '{$user_id}'";
+
+    $update = mysqli_query($connection,$updateUser) or die("Query faild.");
+    if($update){
+      header("location: users.php");
+    }
+}
+
+?>
+  <div id="admin-content">
+      <div class="container">
+          <div class="row">
+              <div class="col-md-12">
+                  <h1 class="admin-heading">Modify User Details</h1>
+              </div>
+              <div class="col-md-offset-4 col-md-4">
+
+            <?php 
+                $user_id = $_GET['id'];
+
+                include "config.php";
+                $findUser = "SELECT * FROM user WHERE user_id = {$user_id}";
+                $result = mysqli_query($connection,$findUser) or die("Failed");
+                $count = mysqli_num_rows($result);
+
+                if($count > 0){ // if start backet
+                while($row = mysqli_fetch_assoc($result)){ // while start backet
+
+            ?>
+                  <!-- Form Start -->
+                  <form  action="<?php $_SERVER['PHP_SELF'] ?>" method ="POST">
+                      <div class="form-group">
+                          <input type="hidden" name="user_id"  class="form-control" value="<?php echo $row['user_id'] ?>" placeholder="" >
+                      </div>
+                          <div class="form-group">
+                          <label>First Name</label>
+                          <input type="text" name="f_name" class="form-control" value="<?php echo $row['first_name'] ?>" placeholder="" required>
+                      </div>
+                      <div class="form-group">
+                          <label>Last Name</label>
+                          <input type="text" name="l_name" class="form-control" value="<?php echo $row['last_name'] ?>" placeholder="" required>
+                      </div>
+                      <div class="form-group">
+                          <label>User Name</label>
+                          <input type="text" name="username" class="form-control" value="<?php echo $row['username'] ?>" placeholder="" required>
+                      </div>
+                      <div class="form-group">
+                          <label>User Role</label>
+                          <select class="form-control" name="role" value="<?php echo $row['role']; ?>">
+                            <?php 
+                                  if($row['role'] == 1){
+                                  
+                                echo  "<option value='0'>Moderator</option>";
+                                echo  "<option value='1' selected >Admin</option>";
+
+                                  }else{
+                                 
+                                 echo  "<option value='0' selected >Moderator</option>";
+                                 echo  "<option value='1'>Admin</option>";
+
+                                  }
+                            ?>
+
+                          </select>
+                      </div>
+                      <input type="submit" name="submit" class="btn btn-primary" value="Update" required />
+                  </form>
+                  <!-- /Form -->
+
+                    <?php 
+                          } // while end backet
+
+                    } // if end backet
+
+                    ?>
+              </div>
+          </div>
+      </div>
+  </div>
+<?php include "footer.php"; ?>
